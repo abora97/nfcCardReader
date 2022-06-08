dc9e071ea54933ff4aa143e319d86f2cad683887
Abora <mahmod.elebiary@gmail.com>
2022-06-08 23:49:08 +0200

    git init

---
+++ app/src/main/java/com/abora/readfromnfc/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.abora.readfromnfc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
