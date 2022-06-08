24dc3828544bf82bb2f2db4afdb133d01d7e04e3
Abora <mahmod.elebiary@gmail.com>
2022-06-09 00:07:12 +0200

    add nfc util

--- app/src/main/java/com/abora/readfromnfc/MainActivity.kt
+++ app/src/main/java/com/abora/readfromnfc/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.abora.readfromnfc
 
+import android.content.Intent
+import android.nfc.NfcAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    private var mNfcAdapter: NfcAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        Toast.makeText(this,NFCUtil.retrieveNFCMessage(this.intent),Toast.LENGTH_LONG).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mNfcAdapter?.let {
+            NFCUtil.enableNFCInForeground(it, this, javaClass)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mNfcAdapter?.let {
+            NFCUtil.disableNFCInForeground(it, this)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        val messageWrittenSuccessfully = NFCUtil.createNFCMessage(messageEditText.text.toString(), intent)
+        Toast.makeText(this,messageWrittenSuccessfully.ifElse("Successful Written to Tag", "Something When wrong Try Again"),Toast.LENGTH_LONG).show()
     }
 }
+
+fun <T> Boolean.ifElse(primaryResult: T, secondaryResult: T) = if (this) primaryResult else secondaryResult
